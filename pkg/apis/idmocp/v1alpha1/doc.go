b84b26f573ab0d4c6ee54144828610f7079daed5
Fraser Tweedale <ftweedal@redhat.com>
2020-07-20 12:48:35 +1000

    operator-sdk add api
    Commit result of executing:
    
      operator-sdk add api \
        --api-version=idmocp.redhat.com/v1alpha1 --kind=IDM

---
+++ pkg/apis/idmocp/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the idmocp v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=idmocp.redhat.com
+package v1alpha1
---
+++ pkg/apis/idmocp/v1alpha1/idm_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// IDMSpec defines the desired state of IDM
+type IDMSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// IDMStatus defines the observed state of IDM
+type IDMStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// IDM is the Schema for the idms API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=idms,scope=Namespaced
+type IDM struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IDMSpec   `json:"spec,omitempty"`
+	Status IDMStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// IDMList contains a list of IDM
+type IDMList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []IDM `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&IDM{}, &IDMList{})
+}
