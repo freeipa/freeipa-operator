43bcb282e49a8fd1564c9911af6d391952aa694d
Fraser Tweedale <ftweedal@redhat.com>
2020-07-20 12:48:35 +1000

    operator-sdk add api
    Commit result of executing:
    
      operator-sdk add api \
        --api-version=idmocp.redhat.com/v1alpha1 --kind=IDM

---
+++ src/idmocp/pkg/apis/idmocp/group.go
@@ -0,0 +1,6 @@
+// Package idmocp contains idmocp API versions.
+//
+// This file ensures Go source parsers acknowledge the idmocp package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package idmocp
