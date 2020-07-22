54cc8f8ecb4d42e60676b52d5a982c51a627ee46
Fraser Tweedale <ftweedal@redhat.com>
2020-07-22 12:53:22 +1000

    types: add basic spec and status fields

--- src/idmocp/pkg/apis/idmocp/v1alpha1/idm_types.go
+++ src/idmocp/pkg/apis/idmocp/v1alpha1/idm_types.go
@@ -9,16 +9,12 @@ import (
 
 // IDMSpec defines the desired state of IDM
 type IDMSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Realm string `json:"realm"`
 }
 
 // IDMStatus defines the observed state of IDM
 type IDMStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Servers []string `json:"servers"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
