816b51d535ea35fb3bdb03f8f5c53366b04a788a
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-01 09:25:59 +0100

    Decouple go-get-tool from Makefile

---
+++ mk/go-get-tool.mk
@@ -0,0 +1,13 @@
+# go-get-tool will 'go get' any package $2 and install it to $1.
+PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+define go-get-tool
+@[ -f $(1) ] || { \
+set -e ;\
+TMP_DIR=$$(mktemp -d) ;\
+cd $$TMP_DIR ;\
+go mod init tmp ;\
+echo "Downloading $(2)" ;\
+GOBIN=$(PROJECT_DIR)/bin go get $(2) ;\
+rm -rf $$TMP_DIR ;\
+}
+endef
