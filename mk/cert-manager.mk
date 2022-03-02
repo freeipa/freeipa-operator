c55aacef71f39dadce554c8d6793c28cc49152cf
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-02 11:17:33 +0100

    Include cert-manager rules and update documentation

--- mk/cert-manager.mk
+++ mk/cert-manager.mk
@@ -1,22 +1,24 @@
 
 CERT_MANAGER_CVS_NAME=$(shell kubectl get Subscription/cert-manager -n openshift-operators -o jsonpath='{.status.installedCSV}')
 
+##@ Cert Manager
+
 .PHONY: cert-manager-install
-cert-manager-install:
+cert-manager-install:  ## Install cert-manager operator
 	kubectl create -f config/certmanager/subscription.yaml
 
 .PHONY: cert-manager-uninstall
-cert-manager-uninstall:
+cert-manager-uninstall:  ## Delete cert-manager operator
 ifneq (,$(CERT_MANAGER_CVS_NAME))
 	kubectl delete ClusterServiceVersion/$(CERT_MANAGER_CVS_NAME) -n openshift-operators
 endif
 	kubectl delete -f config/certmanager/subscription.yaml
 
 .PHONY: cert-manager-self-signed-issuer-create
-cert-manager-self-signed-issuer-create:
+cert-manager-self-signed-issuer-create:  ## Create the self-signed certificate issuer
 	kubectl create -f config/certmanager/clusterissuer-selfsigned.yaml
 
 .PHONY: cert-manager-self-signed-issuer-delete
-cert-manager-self-signed-issuer-delete:
+cert-manager-self-signed-issuer-delete:  ## Delete the self-signed certificate issuer
 	kubectl delete -f config/certmanager/clusterissuer-selfsigned.yaml
 
