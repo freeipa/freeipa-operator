2de3b3fb67d603c034a8ac330b9c08f6f6c7e2c9
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-02 10:56:03 +0100

    Update unit tests to match the service accunt name that is used for the workload

--- manifests/statefulset_test.go
+++ manifests/statefulset_test.go
@@ -190,7 +190,7 @@ var _ = Describe("LOCAL:Statefulset tests", func() {
 						close(done)
 					}()
 				})
-				Expect(result.Spec.Template.Spec.ServiceAccountName).Should(Equal("idm"))
+				Expect(result.Spec.Template.Spec.ServiceAccountName).Should(Equal("freeipa"))
 				Expect(len(result.Spec.Template.Spec.Containers)).Should(Equal(1))
 				Expect(result.Spec.Template.Spec.Containers[0].Name).Should(Equal("main"))
 				Expect(result.Spec.Template.Spec.Containers[0].Image).Should(Equal(workload))
