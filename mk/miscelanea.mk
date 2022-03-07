731885eb8dba1a9cfb0b55e05d547e675c463b9c
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-07 15:19:35 +0100

    Add operator-sdk rule to download it locally

--- mk/miscelanea.mk
+++ mk/miscelanea.mk
@@ -1,6 +1,22 @@
+-include mk/go-get-tool.mk
+
+export PATH:=$(shell pwd)/bin:$(PATH)
+
 ##@ Miscelanea
 
 # https://www.cmcrossroads.com/article/dumping-every-makefile-variable
 .PHONY: printvars
 printvars: ## Print variable name and values
 	@$(foreach V, $(sort $(.VARIABLES)),$(if $(filter-out environment% default automatic,$(origin $V)),$(info $V=$($V))))
+
+OPERATOR_SDK := $(PWD)/bin/operator-sdk
+.PHONY: operator-sdk
+operator-sdk: ## Donwload operator-sdk
+ifeq (,$(wildcard $(OPERATOR_SDK)))
+	set -e ; \
+	export ARCH=$$(case $$(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $$(uname -m) ;; esac) ; \
+	export OS=$$(uname | awk '{print tolower($$0)}') ; \
+	export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.18.0 ; \
+	curl -sSLo "$(OPERATOR_SDK)" "$$OPERATOR_SDK_DL_URL/operator-sdk_$${OS}_$${ARCH}" && \
+	[ -x "$(OPERATOR_SDK)" ] || chmod a+x "$(OPERATOR_SDK)"
+endif
