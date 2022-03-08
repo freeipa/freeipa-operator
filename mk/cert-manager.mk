16d9c78f0c3ad6478394edfb750eb35e249e80a7
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-08 11:01:07 +0100

    Update rule to wait cert-manager is installed

--- mk/cert-manager.mk
+++ mk/cert-manager.mk
@@ -5,6 +5,7 @@
 cert-manager-install:  ## Install cert-manager operator
 	kubectl create -f config/certmanager/subscription.yaml
 	kubectl wait Subscription/cert-manager -n openshift-operators --for=condition=CatalogSourcesUnhealthy=False
+	$(call kubectl-wait-for-value,Succeeded,csv/cert-manager.v1.6.1,.status.phase)
 
 .PHONY: cert-manager-uninstall
 cert-manager-uninstall:  ## Delete cert-manager operator
