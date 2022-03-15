06134faf6d8b13cd3f5837c94a84e09af083718f
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-15 12:34:48 +0100

    Add support to indicate a specific test

--- mk/scorecard.mk
+++ mk/scorecard.mk
@@ -1,8 +1,16 @@
 
 ##@ Scorecard
 
-scorecard-bundle: operator-sdk ## Execute scorecard from bundle/ directory
+scorecard-bundle: operator-sdk ## Execute scorecard from bundle/ directory; You can specify selector eg 'make scorecard-bundle scorecard_selector=test=olm-status-descriptors-test'
+ifneq (,$(scorecard_selector))
+	$(OPERATOR_SDK) scorecard bundle --selector $(scorecard_selector)
+else
 	$(OPERATOR_SDK) scorecard bundle
+endif
 
-scorecard-image: operator-sdk ## Execute scorecard from the generated image (BUNDLE_IMG)
+scorecard-image: operator-sdk ## Execute scorecard from the generated image (BUNDLE_IMG); You can specify selector eg 'make scorecard-image scorecard_selector=test=olm-status-descriptors-test'
+ifneq (,$(scorecard_selector))
+	$(OPERATOR_SDK) scorecard $(BUNDLE_IMG) --selector $(scorecard_selector)
+else
 	$(OPERATOR_SDK) scorecard $(BUNDLE_IMG)
+endif
