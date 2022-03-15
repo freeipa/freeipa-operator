b56a1fa7074ebec3c62f2998ef819547f34f1d7d
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-15 14:58:39 +0100

    Simplify rule for uninstalling the operator by using OLM

--- mk/bundle.mk
+++ mk/bundle.mk
@@ -7,6 +7,4 @@ bundle-install: ## Install the operator into the current cluster and namespace b
 bundle-uninstall: SUBSCRIPTION_NAME=$(shell echo "freeipa-operator.v$(VERSION)-sub" | sed -e 's/\./-/g')
 bundle-uninstall: CLUSTER_SERVICE_VERSION=$(shell echo "freeipa-operator.v$(VERSION)")
 bundle-uninstall: ## Uninstall the Operator from the current cluster and namespace
-	-kubectl delete csv/$(CLUSTER_SERVICE_VERSION) --wait=true
-	-kubectl delete subscriptions.operators.coreos.com "$(SUBSCRIPTION_NAME)" --wait=true
-	-kubectl delete catalogsource/freeipa-operator-catalog --wait=true
+	$(OPERATOR_SDK) cleanup freeipa-operator --namespace "$(WATCH_NAMESPACE)" --delete-all
