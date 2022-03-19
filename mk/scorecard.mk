f25e112c6ceaee6ee27f30ea770d71147dd4f92c
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-19 13:04:57 +0100

    Add rules for scorecard-build and scorecard-push for the scorecard images

--- mk/scorecard.mk
+++ mk/scorecard.mk
@@ -1,6 +1,17 @@
 
 ##@ Scorecard
 
+IMG_SCORECARD=$(IMAGE_TAG_BASE)-scorecard:v$(VERSION)
+
+# https://github.com/operator-framework/operator-sdk/blob/09c3aa14625965af9f22f513cd5c891471dbded2/Makefile#L78
+.PHONY: scorecard-build
+scorecard-build:  ## Build the image for the custom scorecard tests
+	docker build -t $(IMG_SCORECARD) -f images/custom-scorecard-tests/Dockerfile .
+
+.PHONY: scorecard-push
+scorecard-push:  ## Push the image for the custom scorecard tests
+	docker image push $(IMG_SCORECARD)
+
 .PHONY: scorecard-bundle
 scorecard-bundle: operator-sdk ## Execute scorecard from bundle/ directory; You can specify selector eg 'make scorecard-bundle scorecard_selector=test=olm-status-descriptors-test'
 ifneq (,$(scorecard_selector))
