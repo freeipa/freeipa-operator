1d27fb221602204c5c40bb324ca6f4cb34a1a7d6
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-19 16:10:19 +0100

    Add .PHONY for scorecard-bundle and scorecard-image rules

--- mk/scorecard.mk
+++ mk/scorecard.mk
@@ -1,6 +1,7 @@
 
 ##@ Scorecard
 
+.PHONY: scorecard-bundle
 scorecard-bundle: operator-sdk ## Execute scorecard from bundle/ directory; You can specify selector eg 'make scorecard-bundle scorecard_selector=test=olm-status-descriptors-test'
 ifneq (,$(scorecard_selector))
 	$(OPERATOR_SDK) scorecard bundle --selector $(scorecard_selector)
@@ -8,6 +9,7 @@ else
 	$(OPERATOR_SDK) scorecard bundle
 endif
 
+.PHONY: scorecard-image
 scorecard-image: operator-sdk ## Execute scorecard from the generated image (BUNDLE_IMG); You can specify selector eg 'make scorecard-image scorecard_selector=test=olm-status-descriptors-test'
 ifneq (,$(scorecard_selector))
 	$(OPERATOR_SDK) scorecard $(BUNDLE_IMG) --selector $(scorecard_selector)
