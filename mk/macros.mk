7c9d3a60a5838568d73da61812cdb8e051446b66
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-21 10:19:08 +0100

    Use .venv for the pyton dependencies

--- mk/macros.mk
+++ mk/macros.mk
@@ -7,11 +7,12 @@ endif
 
 ifndef post-manifests
 define post-manifests
-@yaml2json config/rbac/role.yaml | jq '.kind="Role"' > config/rbac/role.json \
-&& json2yaml config/rbac/role.json > config/rbac/role.yaml \
+@[ -e .venv ] || $(MAKE) .venv
+@(source .venv/bin/activate; yaml2json config/rbac/role.yaml) | jq '.kind="Role"' > config/rbac/role.json \
+&& (source .venv/bin/activate; json2yaml config/rbac/role.json > config/rbac/role.yaml) \
 && rm -f config/rbac/role.json \
-&& yaml2json config/rbac/role_binding.yaml | jq '.kind="RoleBinding" | .roleRef.kind="Role"' > config/rbac/role_binding.json \
-&& json2yaml config/rbac/role_binding.json > config/rbac/role_binding.yaml \
+&& (source .venv/bin/activate; yaml2json config/rbac/role_binding.yaml) | jq '.kind="RoleBinding" | .roleRef.kind="Role"' > config/rbac/role_binding.json \
+&& (source .venv/bin/activate; json2yaml config/rbac/role_binding.json) > config/rbac/role_binding.yaml \
 && rm -f config/rbac/role_binding.json
 endef
 endif
