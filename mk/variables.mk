8301a7c64d7c90cc9ef7613d0bbce5e72253853e
Alejandro Visiedo <avisiedo@redhat.com>
2022-03-25 22:29:02 +0100

    Fix TEST_DISABLE_LIST variable name

--- mk/variables.mk
+++ mk/variables.mk
@@ -4,6 +4,7 @@ ifeq (true,$(CI))
 $(info For Git Hub Actions the test for the controller are disabled)
 # Disable the controller tests
 TEST_DISABLE_LIST += controller
+export TEST_DISABLE_LIST
 endif
 
 # When running in ci-operator the env var OPENSHIFT_CI is set
@@ -11,9 +12,12 @@ endif
 # on it.
 ifeq (true,$(OPENSHIFT_CI))
 $(warning This should be enabled in the future for the controller)
-# Disable the controller tests
-TEST_DISABLE+=controller
-export TEST_DISABLE
+
+# TODO Actually the ci-operator pipeline does not claim
+#      a cluster; at the moment it is made in the future
+#      the 'controller' should be removed from TEST_DIEBLE_LIST
+TEST_DISABLE_LIST += controller
+export TEST_DISABLE_LIST
 
 # At 'vendor/sigs.k8s.io/controller-runtime/pkg/internal/testing/addr/manager.go:51'
 # we can read the block:
@@ -44,13 +48,10 @@ export USE_EXISTING_CLUSTER
 HOME=/tmp
 export HOME
 
-# FIXME At the moment of writing this lines, KUBECONFIG variable
-#       was empty in ci-operator job, so there are no access to
-#       the cluster for e2e tests and the controller tests.
 endif
 
 # Print out message to warn about the fact that some tests
 # are not being executed
-ifneq (,$(TEST_DISABLE))
-$(info The following tests are disabled: $(TEST_DISABLE))
+ifneq (,$(TEST_DISABLE_LIST))
+$(info The following tests are disabled: $(TEST_DISABLE_LIST))
 endif
