c83711faa7e6b48df2eaacb99e65c3cd4a60ec2c
Alejandro Visiedo <avisiedo@redhat.com>
2022-04-01 17:09:16 +0200

    deploy:Add dependencies to deploy an updated operator with OLM

--- mk/bundle.mk
+++ mk/bundle.mk
@@ -1,5 +1,6 @@
+.NOTPARALLEL: bundle-install
 .PHONY: bundle-install
-bundle-install: ## Install the operator into the current cluster and namespace by using OLM
+bundle-install: docker-push bundle-push catalog-push ## Install the operator into the current cluster and namespace by using OLM
 	$(OPERATOR_SDK) run bundle $(BUNDLE_IMG) --namespace "$(WATCH_NAMESPACE)"
 
 .PHONY: bundle-delete
