51f7c845341b1d8aa2e3ef7bff7a7c2489e428a3
Alejandro Visiedo <avisiedo@redhat.com>
2022-05-17 11:54:42 +0200

    review: A few changes observed during pr review
    - Update README.md steps indicating to be logged in
      as cluster-admin user, and reference to install
      the CRD into the cluster before run the controller
      locally.
    - Launch operator-sdk binary from ./bin directory.
    - Update some occurrencies of kubectl by oc.
    - Bumped cert-manager to 1.7.2 version.

--- mk/cert-manager.mk
+++ mk/cert-manager.mk
@@ -3,17 +3,17 @@
 
 .PHONY: cert-manager-install
 cert-manager-install:  ## Install cert-manager operator
-	kubectl create -f config/certmanager/subscription.yaml
-	kubectl wait Subscription/cert-manager -n openshift-operators --for=condition=CatalogSourcesUnhealthy=False
+	oc create -f config/certmanager/subscription.yaml
+	oc wait Subscription/cert-manager -n openshift-operators --for=condition=CatalogSourcesUnhealthy=False
 
 .PHONY: cert-manager-uninstall
 cert-manager-uninstall:  ## Delete cert-manager operator
-	kubectl delete -f config/certmanager/subscription.yaml --wait=true
+	oc delete -f config/certmanager/subscription.yaml --wait=true
 
 .PHONY: cert-manager-self-signed-issuer-create
 cert-manager-self-signed-issuer-create:  ## Create a cluster self signed issuer
-	kubectl create -f config/certmanager/clusterissuer-selfsigned.yaml
+	oc create -f config/certmanager/clusterissuer-selfsigned.yaml
 
 .PHONY: cert-manager-self-signed-issuer-delete
 cert-manager-self-signed-issuer-delete:  ## Delete the cluster self signed issuer
-	kubectl delete -f config/certmanager/clusterissuer-selfsigned.yaml
+	oc delete -f config/certmanager/clusterissuer-selfsigned.yaml
--- mk/samples.mk
+++ mk/samples.mk
@@ -8,16 +8,16 @@ sample-build:
 
 .PHONY: sample-create
 sample-create: check-password-is-provided  ## Create the IDM sample resource
-	@kubectl get secret/idm-sample &>/dev/null \
-	 || kubectl create secret generic idm-sample \
+	@oc get secret/idm-sample &>/dev/null \
+	 || oc create secret generic idm-sample \
 	          --from-literal=IPA_ADMIN_PASSWORD='$(IPA_ADMIN_PASSWORD)' \
 	          --from-literal=IPA_DM_PASSWORD='$(IPA_DM_PASSWORD)'
 	oc create -f $(SAMPLE)
 
 .PHONY: sample-delete
 sample-delete:  ## Delete the IDM sample resource
-	@!kubectl get secret/idm-sample &>/dev/null \
-	 || kubectl delete secrets/idm-sample
+	@!oc get secret/idm-sample &>/dev/null \
+	 || oc delete secrets/idm-sample
 	oc delete -f $(SAMPLE)
 
 .PHONY: sample-recreate
