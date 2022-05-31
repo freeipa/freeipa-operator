9d6d5f1cd85b47527d88dae8ff59b5daccd7a05b
Alejandro Visiedo <avisiedo@redhat.com>
2022-05-31 09:30:52 +0200

    makefile:Fix selector expansion when filtering scorecard test by 'make scorecard-bundle scorecard_selector=test=simple'
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- mk/scorecard.mk
+++ mk/scorecard.mk
@@ -15,7 +15,7 @@ scorecard-push:  ## Push the image for the custom scorecard tests
 .PHONY: scorecard-bundle
 scorecard-bundle: operator-sdk ## Execute scorecard from bundle/ directory; You can specify selector eg 'make scorecard-bundle scorecard_selector=test=olm-status-descriptors-test'
 ifneq (,$(scorecard_selector))
-	$(OPERATOR_SDK) scorecard bundle --selector $(scorecard_selector)
+	$(OPERATOR_SDK) scorecard bundle --selector='$(scorecard_selector)'
 else
 	$(OPERATOR_SDK) scorecard bundle
 endif
